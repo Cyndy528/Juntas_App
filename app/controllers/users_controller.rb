d6ff6f8e851007471461299ac84adcd98b1e19d7
Cynthia Salinas <csalinas123@gmail.com>
2016-01-29 17:33:28 -0800

    add edit and delete features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,11 @@ class UsersController < ApplicationController
       updated_user_params = user_params 
     end
     @user = User.new(updated_user_params)
-      if @user.save 
-        session[:user_id] = @user.id 
-        redirect_to user_path(@user)
-      else 
-        redirect_to root_path
-      end 
+    if @user.save 
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else 
+      redirect_to root_path 
     end 
   end 
 
@@ -55,23 +54,19 @@ class UsersController < ApplicationController
 
   def destroy 
     if current_user == @user
-      if @user.destroy
+       @user.destroy
         session[:user_id] = nil
         redirect_to root_path
-      else 
-        redirect_to user_path(current_user)
-      end 
     end 
-  end 
+  end
+ 
+  private 
 
+    def user_params
+      params.require(:user).permit(:full_name, :email, :avatar, :password, :interests, :bio, :slug)
+    end 
 
-private 
-
-  def user_params
-    params.require(:user).permit(:full_name, :email, :avatar, :password, :interests, :bio, :slug)
-  end 
-
-  def get_user
-    @user = User.find_by_id(params[:id])
-  end 
-
+    def get_user
+      @user = User.find_by_id(params[:id])
+    end 
+end 
